8aab545e268ab0996a69ba33eab7b61879cc1ae7
Aleksandr Korovkin <teamer777@gmail.com>
2022-06-19 19:35:18 +0400

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as http from "http";
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running now at http://${hostname}:${port}`);
+});
