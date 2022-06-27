ef56586e7ee1de9d1fa920f8b4971b464dae91d0
Aleksandr Korovkin <teamer777@gmail.com>
2022-06-27 06:41:07 +0400

    start effect-ts rewrite

--- dist/src/server.js
+++ dist/src/server.js
@@ -35,97 +35,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import { pipe } from "fp-ts/lib/function";
-// import * as RTE from "fp-ts/ReaderTaskEither";
-// import * as E from "fp-ts/Either";
-// import * as O from "fp-ts/Option";
-const http = __importStar(require("http"));
-// import {
-//   VehiclesUrlDecoder,
-//   vehiclesUrlDecoder,
-// } from "./common/codecs/request-urls-codec";
-// import { CHError } from "./common/errors/CHError";
-// import { mapDecoderError } from "./common/utils/map-decoder-error";
-// import { resolvers } from "./resolvers";
-// import { CarService } from "./services/cars/car-service";
-// import { CHDecoderError } from "./common/errors/CHDecoderError";
-// import { CarMake, CarManufacturers } from "./api/vpic/types";
-// import { CHNetworkError } from "./common/errors/CHNetworkError";
-const api_1 = require("./api/vpic/api");
-const axios_1 = __importDefault(require("axios"));
-const hostname = "127.0.0.1";
+const function_1 = require("fp-ts/lib/function");
+const RTE = __importStar(require("fp-ts/ReaderTaskEither"));
+const resolvers_1 = require("./resolvers");
+const car_service_1 = require("./services/cars/car-service");
+const fastify_1 = __importDefault(require("fastify"));
 const port = 3000;
-// type CombinedError = CHNetworkError | CHDecoderError;
-// type CombinedData = CarMake[] | CarManufacturers[];
-http
-    .createServer((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const { url } = req;
-    if (url) {
-        if (url === "/api/vehicles/makes") {
-            const fetchUrl = (0, api_1.getVpicUrl)("getAllMakes");
-            const result = (yield axios_1.default.get(fetchUrl));
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify((_a = result === null || result === void 0 ? void 0 : result.data) === null || _a === void 0 ? void 0 : _a.Results));
-        }
+const server = (0, fastify_1.default)();
+server.get("/api/vehicles/makes", (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    return (0, function_1.pipe)(resolvers_1.resolvers.vehicles.makes, RTE.match((error) => {
+        throw error;
+    }, (success) => {
+        return success;
+    }), (passDeps) => passDeps(car_service_1.CarService), (invoke) => invoke());
+}));
+server.listen({ port }, (err, address) => {
+    if (err) {
+        server.log.error(err);
+        process.exit(1);
     }
-    else {
-        res.statusCode = 500;
-        res.setHeader("Content-Type", "application/json");
-        res.end("BAD!!!!");
-    }
-}))
-    .listen(port, hostname, () => {
-    console.log(`Server running now at http://${hostname}:${port}`);
+    console.log(`Server listening at ${address}`);
 });
-// http
-//   .createServer((req, res) => {
-//     pipe(
-//       req,
-//       (req) => {
-//         return req.url;
-//       },
-//       O.fromNullable,
-//       E.fromOption(() =>
-//         CHError.of("[server]", new Error("Url should not be a null."))
-//       ),
-//       E.chainW((url) => {
-//         // const t = url.split("/")[1];
-//         return E.right(url);
-//       }),
-//       E.chainW((urlString) => {
-//         // const url = new URL(`http://${req.headers.host}${req.url}`);
-//         return mapDecoderError("server")(vehiclesUrlDecoder.decode(urlString));
-//       }),
-//       RTE.fromEither,
-//       RTE.chainW<CarService, CombinedError, VehiclesUrlDecoder, CombinedData>(
-//         (url) => {
-//           switch (url) {
-//             case "/api/vehicles/makes":
-//               return resolvers.vehicles.makes;
-//             case "/api/vehicles/manufacturers":
-//               return resolvers.vehicles.manufacturers;
-//           }
-//         }
-//       ),
-//       RTE.match(
-//         (error) => {
-//           res.statusCode = 500;
-//           res.setHeader("Content-Type", "application/json");
-//           res.end(JSON.stringify(error));
-//         },
-//         (data) => {
-//           res.statusCode = 200;
-//           res.setHeader("Content-Type", "application/json");
-//           res.end(JSON.stringify(data));
-//         }
-//       ),
-//       (passDeps) => passDeps(CarService),
-//       (invoke) => invoke()
-//     );
-//   })
-//   .listen(port, hostname, () => {
-//     console.log(`Server running now at http://${hostname}:${port}`);
-//   });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,97 +1,33 @@
-// import { pipe } from "fp-ts/lib/function";
-// import * as RTE from "fp-ts/ReaderTaskEither";
-// import * as E from "fp-ts/Either";
-// import * as O from "fp-ts/Option";
-import * as http from "http";
-// import {
-//   VehiclesUrlDecoder,
-//   vehiclesUrlDecoder,
-// } from "./common/codecs/request-urls-codec";
-// import { CHError } from "./common/errors/CHError";
-// import { mapDecoderError } from "./common/utils/map-decoder-error";
-// import { resolvers } from "./resolvers";
-// import { CarService } from "./services/cars/car-service";
-// import { CHDecoderError } from "./common/errors/CHDecoderError";
-// import { CarMake, CarManufacturers } from "./api/vpic/types";
-// import { CHNetworkError } from "./common/errors/CHNetworkError";
-import { getVpicUrl } from "./api/vpic/api";
-import axios, { AxiosResponse } from "axios";
+import { pipe } from "fp-ts/lib/function";
+import * as RTE from "fp-ts/ReaderTaskEither";
+import { resolvers } from "./resolvers";
+import { CarService } from "./services/cars/car-service";
+import fastify from "fastify";
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
-// type CombinedError = CHNetworkError | CHDecoderError;
-// type CombinedData = CarMake[] | CarManufacturers[];
+const server = fastify();
 
-http
-  .createServer(async (req, res) => {
-    const { url } = req;
-    if (url) {
-      if (url === "/api/vehicles/makes") {
-        const fetchUrl = getVpicUrl("getAllMakes");
-        const result = (await axios.get<unknown, AxiosResponse<unknown>>(
-          fetchUrl
-        )) as any;
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(result?.data?.Results));
+server.get("/api/vehicles/makes", async (request, reply) => {
+  return pipe(
+    resolvers.vehicles.makes,
+    RTE.match(
+      (error) => {
+        throw error;
+      },
+      (success) => {
+        return success;
       }
-    } else {
-      res.statusCode = 500;
-      res.setHeader("Content-Type", "application/json");
-      res.end("BAD!!!!");
-    }
-  })
-  .listen(port, hostname, () => {
-    console.log(`Server running now at http://${hostname}:${port}`);
-  });
+    ),
+    (passDeps) => passDeps(CarService),
+    (invoke) => invoke()
+  );
+});
 
-// http
-//   .createServer((req, res) => {
-//     pipe(
-//       req,
-//       (req) => {
-//         return req.url;
-//       },
-//       O.fromNullable,
-//       E.fromOption(() =>
-//         CHError.of("[server]", new Error("Url should not be a null."))
-//       ),
-//       E.chainW((url) => {
-//         // const t = url.split("/")[1];
-//         return E.right(url);
-//       }),
-//       E.chainW((urlString) => {
-//         // const url = new URL(`http://${req.headers.host}${req.url}`);
-//         return mapDecoderError("server")(vehiclesUrlDecoder.decode(urlString));
-//       }),
-//       RTE.fromEither,
-//       RTE.chainW<CarService, CombinedError, VehiclesUrlDecoder, CombinedData>(
-//         (url) => {
-//           switch (url) {
-//             case "/api/vehicles/makes":
-//               return resolvers.vehicles.makes;
-//             case "/api/vehicles/manufacturers":
-//               return resolvers.vehicles.manufacturers;
-//           }
-//         }
-//       ),
-//       RTE.match(
-//         (error) => {
-//           res.statusCode = 500;
-//           res.setHeader("Content-Type", "application/json");
-//           res.end(JSON.stringify(error));
-//         },
-//         (data) => {
-//           res.statusCode = 200;
-//           res.setHeader("Content-Type", "application/json");
-//           res.end(JSON.stringify(data));
-//         }
-//       ),
-//       (passDeps) => passDeps(CarService),
-//       (invoke) => invoke()
-//     );
-//   })
-//   .listen(port, hostname, () => {
-//     console.log(`Server running now at http://${hostname}:${port}`);
-//   });
+server.listen({ port }, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
